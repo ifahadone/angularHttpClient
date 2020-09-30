b170b6193bb84be131ee994899da4cbc223be12b
mnizfahad <md.nizfahad@gmail.com>
2020-09-30 11:18:44 +0800

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Serve static files....
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angularHttpClient'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angularHttpClient/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/angularHttpClient/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
-
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 
